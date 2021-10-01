0b0426fbb31b9fca2032848e2127533f56e7c2ea
FHaggs <pedrofamh@protonmail.com>
2021-10-01 10:01:04 -0400

    paddle class

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
+import pygame
 
+WIN_WIDTH = 490
+WIN_HEIGHT = 370
 
 class Paddle:
 
@@ -6,8 +9,46 @@ class Paddle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    def move:
+        self.width = 5
+        self.heigth = WIN_HEIGHT/10
 
-class Ball:
+        self.vel = 0
+        self.rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move_up(self):
     
+        self.vel -5
+    
+    def move_down(self):
+
+        self.vel = 5
+
+    def move(self):
+        # Check if is hiting the top or bottom of the screen
+        if self.rectangle.top => WIN_HEIGHT and self.vel > 0:
+            self.vel = 0
+        elif self.rectangle.top => 0 and self.vel > 0:
+            self.vel = 0
+
+        self.rectangle = self.rectangle.move([0, self.vel])
+    
+    def draw(self, screen):
+
+        pygame.draw.rect(screen, (255,255,255), self.rectangle)
+        
+    
+        
+class Ball:
+
+
+
+def draw_screen():
+
+
+
+def eval_genomes():
+
+def run(config_file):
+
+def __main__():
+
